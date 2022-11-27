2518fb592f8b8e0e28291efdcb50b1ace7b4abcc
Mateusz Kretowski <matkretkurs@gmail.com>
2022-11-27 23:59:35 +0100

    Add method initAccordion for class Product

--- src/js/script.js
+++ src/js/script.js
@@ -58,6 +58,7 @@
       thisProduct.id = id;
       thisProduct.data = data;
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
       console.log('New Product', thisProduct);
     }
     renderInMenu() {
@@ -71,7 +72,29 @@
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
     }
+    initAccordion() {
+      const thisProduct = this;
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      //console.log('clicked', clickableTrigger);
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function (event) {
+        /* prevent default action for event */
+        event.preventDefault();
+        /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(select.all.menuProductsActive);
+        /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if (activeProduct != null && activeProduct != thisProduct.element) {
+          activeProduct.classList.toggle('active');
+          thisProduct.element.classList.toggle('active');
+        } else {
+          /* toggle active class on thisProduct.element */
+          thisProduct.element.classList.toggle('active');
+        }
+      });
+    }
   }
+
   const app = {
     initMenu: function () {
       const thisApp = this;
@@ -83,7 +106,6 @@
       /*const testProduct = new Product();
       console.log('testProduct:', testProduct);*/
     },
-
     initData: function () {
       const thisApp = this;
       thisApp.data = dataSource;
