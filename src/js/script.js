ee69841470c29b6de93f2a17571cd2f1c7f4eb7c
Mateusz Kretowski <matkretkurs@gmail.com>
2022-12-01 07:38:09 +0100

    Fix bugs

--- src/js/script.js
+++ src/js/script.js
@@ -172,11 +172,11 @@
     },
     init: function () {
       const thisApp = this;
-      //console.log('*** App starting ***');
-      //console.log('thisApp:', thisApp);
-      //console.log('classNames:', classNames);
-      //console.log('settings:', settings);
-      //console.log('templates:', templates);
+      console.log('*** App starting ***');
+      console.log('thisApp:', thisApp);
+      console.log('classNames:', classNames);
+      console.log('settings:', settings);
+      console.log('templates:', templates);
       thisApp.initData();
       thisApp.initMenu();
     },
